7aad62d8f36220efdc1c8fe097d98599e2ea1ed3
ianjamesburke <37986506+ianjamesburke@users.noreply.github.com>
2024-10-18 00:44:40 -0700

    facepalm...

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 from app import app
 
-def teset_hello_world_route():
+def test_hello_world_route():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
